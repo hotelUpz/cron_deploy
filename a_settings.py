1b501b3e074295a6dab8bb06f91c13fec0b5d1bc
kali <kali@kali.netis.cc>
2025-10-01 17:06:34 +0300

    ch lim

--- a_settings.py
+++ a_settings.py
@@ -53,8 +53,8 @@ class UsersSettings():
                 "margin_type": "CROSSED",         # Тип маржи. Кросс-маржа → "CROSSED", Изолированная → "ISOLATED"
                 "quote_asset": "USDT",            # → валюта, в которой указана цена (например, USDT, USDC, BUSD)
                 "direction": 3,                   # 1 -- LONG, 2 --SHORT, 3 -- BOTH
-                "long_positions_limit": 1,        # количество одновременно открываемых лонгов
-                "short_positions_limit": 1,       # количество одновременно открываемых шортов
+                "long_positions_limit": 2,        # количество одновременно открываемых лонгов
+                "short_positions_limit": 2,       # количество одновременно открываемых шортов
             },
 
             "symbols_risk": {
